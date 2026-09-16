3adc3dbd0b066b645c51bc268d54a1bcd0834c97
karthik96siddhu <karthik96siddhu@gmail.com>
2026-09-16 13:04:29 +0530

    update collection name for vdo-xperts customer

--- config/db.py
+++ config/db.py
@@ -9,4 +9,4 @@ cluster = MongoClient(os.getenv('MONGO_URL'))
 db = cluster[os.getenv('DB_NAME')]
 collection = db[os.getenv('COLLECTION_NAME')]
 admin_collection = db[os.getenv('ADMIN_COLLECTION_NAME')]
-customer_contact_collection = db[os.getenv('CUSTOMER_CONTACT_COLLECTION_NAME', 'customer_contacts')]
+customer_contact_collection = db[os.getenv('VDO_XPERTS_CUSTOMER_COLLECTION_NAME', 'vdo-xperts-customer')]
